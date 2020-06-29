7f1959a3a71d10aa664f5b0b19f5ba4edc8bd9d8
robert-walsh-hrri <rwalsh@gmail.com>
2020-06-29 18:26:40 -0500

    Additional skeleton, helper fx stubs

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,23 +4,44 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  for (var i = 0; i < json.length; i++) {
+    if (character === {) {
+      objectParser(json.slice(i +1, json.length -1));
+    }
+  }
+
+  // check the current character and see if its {}[],:' "
+
+    // send to the appropriate helper function
+
+
+  // next character function -
+    // keeps track of place in string
+    // removes whitespace
+
   // wrap object function
+  var objectParser = function (json) {
     // closure function???
+    if (json === '{}') return {};
+    parseJSON()
     // this one would contain the comma and colon character cases
     // calls wrap string function
     // this function recurs parseJSON
     // returns the object that is built
+    return /* something that represents the recurred object evaluation */;
+  }
   // wrap array function
+  var arrayParser = function () {
     // closure function ???
     // this one would contain the comma case
     // this function recurs parseJSON
     // returns the actual array that is built
+  }
   // wrap string function ***
+  var stringParser = function () {
     // this one contains the ' " cases
+  }
 
-
-  // check the current character and see if its {}[],:' "
-    // send to the appropriate helper function
   /*
   object
     {}
@@ -47,9 +68,7 @@ var parseJSON = function(json) {
       null
   */
 
-  // next character function -
-    // keeps track of place in string
-    // removes whitespace
+
 
 
 };
