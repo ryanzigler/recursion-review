d5635c6043debacca4a9bbf5ece92fc245202f3f
robert-walsh-hrri <rwalsh@gmail.com>
2020-06-29 17:25:37 -0500

    getElementsByClassName tests passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
-};
+  // create a result variable
+  var resultArray = [];
+
+  // function that recurs
+  var searchInElements = function (node) {
+    // test to see whether or not className is present
+    if (node.classList) {
+    // push to the result array
+      if (node.classList.contains(className)) {
+        resultArray.push(node);
+      }
+    }
+    // conditional to see if there are children if so recurse
+    if (node.childNodes) {
+      // iteration over the childnodes and recurse on the childnodes
+     _.each(node.childNodes, (item) => {
+        searchInElements(item);
+      });
+    }
+  }
+  // initialize the function with document.body
+  searchInElements(document.body);
+  // return the result variable of arrays
+  return resultArray;
+}
